50e5524e2d3cfbf5b654396f4e1c0be4ba0b4f54
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-08-25 18:53:33 -0300

    Tenta passar nos requisitos 12 a 19

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const inputEmail = document.querySelector('[name=email]');
 const inputPassword = document.querySelector('[name=password]');
 const buttonLogin = document.querySelector('#confirmLogin');
+const rateButtons = document.querySelector('#third');
+const agreementCheck = document.querySelector('#agreement');
+const submitButton = document.querySelector('#submit-btn');
+
+function createRateButtons() {
+  for (let i = 1; i <= 10; i += 1) {
+    const createInput = document.createElement('input');
+    const createSpan = document.createElement('span');
+    createSpan.innerText = i;
+    createInput.type = 'radio';
+    createInput.name = 'rate';
+    createInput.value = i;
+    rateButtons.appendChild(createSpan);
+    rateButtons.appendChild(createInput);
+  }
+}
+
+createRateButtons();
 
 buttonLogin.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
@@ -9,3 +27,19 @@ buttonLogin.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('estou funcionando');
+});
+
+function disableSubmitButton() {
+  if (agreementCheck.checked !== true) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
+}
+
+disableSubmitButton();
+agreementCheck.addEventListener('click', disableSubmitButton);
